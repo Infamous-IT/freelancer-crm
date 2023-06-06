77d00734b75af02e8234a3ae8b5a4d5800b4fbb4
infamous-it <gluhanuk98@gmail.com>
2023-06-06 20:24:46 +0300

    Added Swagger

--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUI from 'swagger-ui-express';
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 dotenv.config();
@@ -17,12 +19,25 @@ const connect = async () => {
     }
 }
 
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Freelancer CRM API',
+            version: '1.0.0'
+        },
+    },
+    apis: ['./routes/*.js'],
+};
+
 mongoose.connection.on('Disconnected', () => {
     console.log('MongoDB was disconnected!');
 })
 
+const swaggerSpec = swaggerJSDoc(options);
 app.use(cors());
 app.use(express.json());
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 
 app.listen(PORT, () => {
